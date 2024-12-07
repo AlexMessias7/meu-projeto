d5e8af30229e037c00ebcb1bf870512e9b486297
Seu Nome <seu-email@example.com>
2024-12-06 21:41:22 -0300

    Atualiza requirements.txt para dependências mínimas

--- minha_empresa/minha_empresa/settings.py
+++ minha_empresa/minha_empresa/settings.py
@@ -79,10 +79,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
